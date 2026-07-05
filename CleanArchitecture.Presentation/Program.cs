62b08eed5e55b5593f72c5fa87072df5e40b73c4
Yeganeh <yeganeh.rad434@gmail.com>
2026-07-05 10:10:49 +0330

    Configure database connection and dependency injection

--- CleanArchitecture.Presentation/Program.cs
+++ CleanArchitecture.Presentation/Program.cs
@@ -1,6 +1,21 @@
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infrastructure.Data;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+);
+
+builder.Services.AddScoped<IUserRepository,UserRepository>();
+builder.Services.AddScoped<INoteRepository,NoteRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
